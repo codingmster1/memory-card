c2ccc252e612f3e5e3b6ed160ea4844287d81f73
Jeff <doylejeff98@gmail.com>
2023-10-27 13:53:41 -0400

    new assets and characters.js with const

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+import './styles/font.css';
+import "./styles/index.css";
+
+function App() {
+
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/characters.js
@@ -0,0 +1,76 @@
+import uniqid from 'uniqid';
+import guts from './assets/guts-GA.webp';
+import casca from './assets/casca-GA.png';
+import griffith from './assets/Griffith_Card_GA.webp';
+import zodd from './assets/zodd-fly.png';
+import Void from './assets/Void.webp';
+import slan from './assets/slan.png';
+import conrad from './assets/Conrad_Manga.webp';
+import count from './assets/count.webp';
+import femto from './assets/femto.jpg';
+import skullknight from './assets/skull-knight.jpg';
+
+const characters = [
+    {
+        id: uniqid(),
+        name: 'Guts',
+        src: guts,
+        clicked: false,
+    },
+    {
+        id: uniqid(),
+        name: 'Casca',
+        src: casca,
+        clicked: false,
+    },
+    {
+        id: uniqid(),
+        name: 'Griffith',
+        src: griffith,
+        clicked: false,
+    },
+    {
+        id: uniqid(),
+        name: 'Zodd',
+        src: zodd,
+        clicked: false,
+    },
+    {
+        id: uniqid(),
+        name: 'Void',
+        src: Void,
+        clicked: false,
+    },
+    {
+        id: uniqid(),
+        name: 'Slan',
+        src: slan,
+        clicked: false,
+    },
+    {
+        id: uniqid(),
+        name: 'conrad',
+        src: conrad,
+        clicked: false,
+    },
+    {
+        id: uniqid(),
+        name: 'Count',
+        src: count,
+        clicked: false,
+    },
+    {
+        id: uniqid(),
+        name: 'Femto',
+        src: femto,
+        clicked: false,
+    },
+    {
+        id: uniqid(),
+        name: 'skull knight',
+        src: skullknight,
+        clicked: false,
+    },
+];
+
+export default characters;
