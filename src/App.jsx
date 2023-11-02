d64bce5109704ada321b177debd6a7fbf580c714
Jeff <doylejeff98@gmail.com>
2023-11-02 00:27:57 -0400

    removed unneeded imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Game from "./components/Game";
+
 
 import './styles/font.css';
 import "./styles/index.css";
-import "./styles/general.css";
+
 
 
 function App() {
   return (
     <div >
-      <Game />
+
     </div>
 
   )
