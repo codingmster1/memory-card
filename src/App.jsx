d3948aef24feab88a2cdb7f3f240830a2bf9f7cf
Jeff <doylejeff98@gmail.com>
2023-11-02 00:44:26 -0400

    imported theme music and click sound

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 
-
+import { useState, useEffect } from 'react'
+import ReactHowler from 'react-howler'
+import ClickSound from './assets/music/clickeffect.wav'
+import BerserkTheme from './assets/music/FORCES.mp3'
+import './styles/App.css'
 import './styles/font.css';
 import "./styles/index.css";
 
