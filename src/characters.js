cedd5db0b30a161f5ac52ded2bbde23890581910
Jeff <doylejeff98@gmail.com>
2023-11-02 02:42:38 -0400

    9 new asset cards and added new uniqids for const

--- src/characters.js
+++ src/characters.js
@@ -25,5 +25,230 @@ import isidro from './assets/Isidro.webp';
 import serpico from './assets/Serpico_wind.webp';
 import rodrick from './assets/rodrick.jpg';
 import farnese from './assets/Farnese_Manga.webp';
+import corkus from './assets/corkus.png';
+import grunbeld from './assets/Grunbeld.webp';
+import irvine from './assets/Irvine_Manga.webp';
+import sonia from './assets/Sonia_Manga.webp';
+import rakshas from './assets/rakshas.webp';
+import locus from './assets/locus.png';
+import ganishka from './assets/ganishka.jpeg';
+import gutsBlackSword from './assets/Guts_Berserker_Manga.webp';
+import casca2 from './assets/casca-2.png';
+
+const berserkCharacters = [
+    {
+        id: uniqid(),
+        name: "guts",
+        src: guts,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "casca",
+        src: casca,
+        clicked: false,
+    },
+    {
+        id: uniqid(),
+        name: "griffith",
+        src: griffith,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "pippin",
+        src: pippin,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "judea",
+        src: judea,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "zodd",
+        src: zodd,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "wylad",
+        src: wylad,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "rickert",
+        src: rickert,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "silat",
+        src: silat,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "skull knight",
+        src: skullknight,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "void",
+        src: viod,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "slan",
+        src: slan,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "conrad",
+        src: conrad,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "count",
+        src: count,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "Snake Apostle",
+        src: snakeApostle,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "femto",
+        src: femto,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "guts armor",
+        src: gutsArmor,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "beast",
+        src: beast,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "behelit",
+        src: behelit,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "puck",
+        src: puck,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "schreike",
+        src: schreike,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "gaiseric",
+        src: gaiseric,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "isidro",
+        src: isidro,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "serpico",
+        src: serpico,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "rodrick",
+        src: rodrick,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "farnese",
+        src: farnese,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "corkus",
+        src: corkus,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "grunbeld",
+        src: grunbeld,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "irvine",
+        src: irvine,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "sonia",
+        src: sonia,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "rakshas",
+        src: rakshas,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "locus",
+        src: locus,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "ganishka",
+        src: ganishka,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "guts",
+        src: gutsBlackSword,
+        clicked: false
+    },
+    {
+        id: uniqid(),
+        name: "casca",
+        src: casca2,
+        clicked: false
+    },
+
+];
+
+export { berserkCharacters };
 
 
